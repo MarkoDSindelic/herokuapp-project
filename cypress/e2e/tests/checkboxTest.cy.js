d64aecaac31077bb1bfbb61f05d39327cdbd21f8
MarkoDSindelic <markods@aia.ac>
2024-09-20 12:55:57 +0200

    checkbox tests revision

--- cypress/e2e/tests/checkboxTest.cy.js
+++ cypress/e2e/tests/checkboxTest.cy.js
@@ -7,18 +7,26 @@ describe ('checkbox test', ()=> {
         cy.visit(Cypress.env('URL') + 'checkboxes')
     });
 
-    
-    
-    
-    it('checkbox click', () => {
-        checkboxPageObj.clickCheckbox()
-        checkboxPageObj.webElements.checkbox().should('be.checked')
+     
+
+    it('check checkbox1 ', () => {
+        checkboxPageObj.clickCheckboxOne()
+        checkboxPageObj.webElements.checkbox1().should('be.checked')
     });
 
-    it('checkbox unclick', () => {
-        checkboxPageObj.unclickCheckbox()
-        checkboxPageObj.webElements.checkbox().should('not.be.checked')
+    it('uncheck checkbox1', () => {
+        checkboxPageObj.unclickCheckboxOne()
+        checkboxPageObj.webElements.checkbox1().should('not.be.checked')
     });
 
+    it('uncheck checkbox2', () => {
+        checkboxPageObj.unclickCheckboxTwo()
+        checkboxPageObj.webElements.checkbox2().should('not.be.checked')
+    });
+
+    it('check checkbox2 ', () => {
+        checkboxPageObj.clickCheckboxTwo()
+        checkboxPageObj.webElements.checkbox2().should('be.checked')
+    });
 
 } )
--- cypress/pages/checkboxesPage.js
+++ cypress/pages/checkboxesPage.js
@@ -1,19 +1,28 @@
 export class CheckboxPage {
 
     webElements = {
-        checkbox: () => cy.get('input[type=checkbox]')
+        checkbox: () => cy.get('input[type=checkbox]'),
+        checkbox1: () => cy.get('#checkboxes > :nth-child(1)'),
+        checkbox2: () => cy.get('#checkboxes > :nth-child(3)')
         
     }
 
 
-    clickCheckbox(){
-    
-        this.webElements.checkbox().check()
-           
+
+    clickCheckboxOne(){
+        this.webElements.checkbox1().check()
     }
 
-    unclickCheckbox(){
-        
-        this.webElements.checkbox().uncheck() 
+    unclickCheckboxOne(){
+        this.webElements.checkbox1().uncheck()
     }
+
+     clickCheckboxTwo(){
+        this.webElements.checkbox2().check()
+     }
+
+     unclickCheckboxTwo(){
+        this.webElements.checkbox2().uncheck()
+    }
+
 }
